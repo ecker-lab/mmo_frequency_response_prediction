0a4783fd862b3f57c4682754a80d4680e554fe96
Julius Schultz <j.schultz@tu-braunschweig.de>
2023-09-07 10:46:56 +0200

    Added requirements txt and installation via pip

--- Readme.md
+++ Readme.md
@@ -6,12 +6,27 @@ TODO Add abstract
 TODO add Link to Journal article
 
 ## Setup 
+
+To perform the training on gpus please make sure to have a cuda-capable system (https://developer.nvidia.com/cuda-zone)
+
+### Using Anaconda
 We recommend Anaconda to set up the python environment. If Anaconda is installed and activated, run the following to setup the environment acoustics_mmo:
 
 ```
 source setup.sh
 ```
+### Using pip
+To install pytorch and the additional dependencies run
 
+```
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+pip install -r requirements.txt
+```
+
+And then to install the mmo package as an editable install run 
+``` 
+pip install -e .
+```
 
 ## Train
 
@@ -21,7 +36,7 @@ to run a single training:
 
 ```
 cd acousticnn/mmo
-python run.py --config configs/implicit_mlp.yaml --dir path/to/save_directory --encoding none --seed 0 --device cuda
+python run.py --config implicit_mlp.yaml --dir path/to/save_directory --encoding none --seed 0 --device cuda
 ```
 
 the --wildcard arg specifies the number of samples. The --encoding arg specifies whether the input parameters are encoded with a sinosoidal embedding function. To use the scalar parameters, choose --encoding none.
--- acousticnn/mmo/run.sh
+++ acousticnn/mmo/run.sh
@@ -1,16 +1,16 @@
 
 # SEED=0
-# python run.py --config implicit_mlp.yaml --dir arch/no_encoding/implicitmlp --encoding none --seed $SEED
-# python run.py --config implicit_transformer.yaml --dir arch/no_encoding/implicit_transformer --encoding none --seed $SEED
-# python run.py --config explicit_mlp.yaml --dir arch/no_encoding/explicitmlp --encoding none --seed $SEED
-# python run.py --config explicit_transformer.yaml --dir arch/no_encoding/explicit_transformer --encoding none --seed $SEED
+# python run.py --config query_mlp.yaml --dir arch/no_encoding/query_mlp --encoding none --seed $SEED
+# python run.py --config query_transformer.yaml --dir arch/no_encoding/query_transformer --encoding none --seed $SEED
+# python run.py --config grid_mlp.yaml --dir arch/no_encoding/grid_mlp --encoding none --seed $SEED
+# python run.py --config grid_transformer.yaml --dir arch/no_encoding/grid_transformer --encoding none --seed $SEED
 
-# python run_mass_variation.py --config implicit_transformer.yaml --encoding none --dir mass_variation --seed $SEED
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/100 --encoding none --seed $SEED --wildcard 100
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/250 --encoding none --seed $SEED --wildcard 250
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/500 --encoding none --seed $SEED --wildcard 500
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/1000 --encoding none --seed $SEED --wildcard 1000
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/1500 --encoding none --seed $SEED --wildcard 1500
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/2000 --encoding none --seed $SEED --wildcard 2000
-# python run_data_variation.py --config implicit_transformer.yaml --dir data_variation/8000 --encoding none --seed $SEED --wildcard 8000
+# python run_mass_variation.py --config query_transformer.yaml --encoding none --dir mass_variation --seed $SEED
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/100 --encoding none --seed $SEED --wildcard 100
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/250 --encoding none --seed $SEED --wildcard 250
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/500 --encoding none --seed $SEED --wildcard 500
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/1000 --encoding none --seed $SEED --wildcard 1000
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/1500 --encoding none --seed $SEED --wildcard 1500
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/2000 --encoding none --seed $SEED --wildcard 2000
+# python run_data_variation.py --config query_transformer.yaml --dir data_variation/8000 --encoding none --seed $SEED --wildcard 8000
 
